911453131ec1f4721078a26f0859640100e0a6a4
Rohit Kumar <rohitgemini1992@gmail.com>
2025-04-25 17:00:20 +0530

    port update

--- index.js
+++ index.js
@@ -30,6 +30,6 @@ app.get("/api/whoami", function (req, res) {
 });
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT || 3000, function () {
+var listener = app.listen(process.env.PORT || 5500, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
